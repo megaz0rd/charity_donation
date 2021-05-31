3e14fbb51b48dd6745388412192faaaddfb86ff0
megaz0rd <akropiewnicki@gmail.com>
2021-05-31 22:17:40 +0200

    3 Create registration view

--- sharegood/views.py
+++ sharegood/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
 
 from sharegood.models import Donation, Institution
 
@@ -30,5 +32,17 @@ class Login(TemplateView):
     template_name = "login.html"
 
 
-class Register(TemplateView):
+class Register(CreateView):
+    model = get_user_model()
+    fields = ['email', 'password', 'first_name', 'last_name']
     template_name = "register.html"
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.first_name = form.cleaned_data.get("first_name")
+        user.last_name = form.cleaned_data.get("last_name")
+        user.username = form.cleaned_data.get('email')
+        user.set_password(form.cleaned_data.get('password'))
+        user.save()
+        return super(Register, self).form_valid(form)
