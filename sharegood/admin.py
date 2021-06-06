0a77d7e4ba00942a49d60a973f70f3bc469bb5d6
megaz0rd <akropiewnicki@gmail.com>
2021-06-06 15:51:06 +0200

    6, 7, 8 Created Admin Panel

--- sharegood/admin.py
+++ sharegood/admin.py
@@ -3,6 +3,31 @@ from django.contrib import admin
 from sharegood.models import Category, Donation, Institution, CustomUser
 
 admin.site.register(Category)
-admin.site.register(Institution)
-admin.site.register(Donation)
-admin.site.register(CustomUser)
+
+
+class InstitutionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'institution_type')
+    search_fields = ('name', 'categories', 'institution_type')
+    readonly_fields = ('id',)
+
+
+admin.site.register(Institution, InstitutionAdmin)
+
+
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'institution', 'quantity', 'pick_up_date', 'city')
+    search_fields = ('institution', 'categories', 'pick_up_date', 'city',
+                     'user')
+    readonly_fields = ('id',)
+
+
+admin.site.register(Donation, DonationAdmin)
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'first_name', 'last_name')
+    search_fields = ('username', 'email')
+    readonly_fields = ('id',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
